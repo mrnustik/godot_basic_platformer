4f35ed9e071da66bffe6f378a547795f9936073e
Michal Mrnuštík <michal.mrnustik@outlook.com>
2023-01-27 17:41:22 +0100

    Add support for playing animations in editor

--- src/Enemies/FlyBomb/FlyBomb.gd
+++ src/Enemies/FlyBomb/FlyBomb.gd
@@ -1,3 +1,4 @@
+@tool
 extends Path2D
 
 enum AnimationType {
@@ -5,11 +6,20 @@ enum AnimationType {
 	Bounce
 }
 
-@export var animationType: AnimationType
+@export var animationType: AnimationType:
+	set(value): 
+		animationType = value
+		play_current_animation()
+
 @onready var animationPlayer: AnimationPlayer = get_node("AnimationPlayer")
 
-
 func _ready():
+	play_current_animation()
+
+func play_current_animation():
+	if animationPlayer == null:
+		return
+	
 	match(animationType):
 		AnimationType.Loop: animationPlayer.play("MoveLoop")
 		AnimationType.Bounce: animationPlayer.play("MoveBounce")
